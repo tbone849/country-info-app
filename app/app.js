c46de2c9c50e060fcfbd1ffa500c96651d6bfaa0
Tim Sarvey <timsarvey@gmail.com>
2015-12-03 19:17:50 -0500

    Add capital, country, and neighbor ajax calls to details view.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ angular.module('countriesApp', ['ngRoute'])
         }).when('/countries', {
         	templateUrl : 'countries.html',
         	controller : 'country-controller'
-        }).when('/countries/:country/capitol', {
+        }).when('/countries/:country/capital', {
         	templateUrl : 'detail.html',
         	controller : 'country-detail-controller'
         }).otherwise('/');
@@ -15,12 +15,43 @@ angular.module('countriesApp', ['ngRoute'])
             return $http.get('http://api.geonames.org/countryInfoJSON?username=tbone849', {cache:true});
         };
     }])
-    .controller('country-controller', ['$scope', 'countriesRequest', function($scope, countriesRequest){
-        countriesRequest().then(function(response){
-            $scope.data = response.data;
-            console.log($scope.data);
-        });
+    .factory('countryRequest', ['$http', function($http){
+        return function(country){
+            return $http.get('http://api.geonames.org/countryInfoJSON?username=tbone849&country=' + country);
+        };
     }])
-    .controller('country-detail-controller', ['$scope', function($scope){
+    .factory('capitalRequest', ['$http', function($http){
+        return function(country){
+            return $http.get('http://api.geonames.org/searchJSON?q=capital&maxRows=1&username=tbone849&country=' + country);
+        };
+    }])
+    .factory('neighborsRequest', ['$http', function($http){
+        return function(country){
+            return $http.get('http://api.geonames.org/neighboursJSON?username=tbone849&country=' + country);
+        };
+    }])
+    .controller('country-controller', ['$scope', '$location', 'countriesRequest', function($scope, $location, countriesRequest){
+        countriesRequest().then(function(response){
+            $scope.data = response.data.geonames;
+        });
 
+        $scope.setCapitalURL = function(country){
+            $location.path('/countries/' + country.countryCode + '/capital');
+        };
+    }])
+    .controller('country-detail-controller', ['$scope', '$http', '$routeParams', 'capitalRequest', 'neighborsRequest', 'countryRequest', function($scope, $http, $routeParams, capitalRequest, neighborsRequest, countryRequest){
+        
+        var country = $routeParams.country;
+        capitalRequest(country).then(function(response){
+            $scope.capital = response.data.geonames[0];
+            console.log($scope.capital);
+        });
+        neighborsRequest(country).then(function(response){
+            $scope.neighbors = response.data;
+            console.log($scope.neighbors);
+        });
+        countryRequest(country).then(function(response){
+            $scope.country = response.data.geonames[0];
+            console.log($scope.country);
+        });
     }]);
